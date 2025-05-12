a9b1b12a13e7f267e52a9b08274a0d19c15c8c27
nick-cramer11 <nicholascramer53@gmail.com>
2025-05-12 15:04:48 -0700

    Working on lab

--- nc_lab5_functions.py
+++ nc_lab5_functions.py
@@ -6,8 +6,8 @@
 import os, sys
 import rasterio
 import geopandas as gpd
-
-
+import numpy as np
+from rasterstats import zonal_stats
 
 
 ##################
@@ -16,9 +16,9 @@ import geopandas as gpd
 
 # Change this to the directory where your data are
 
-data_dir = "/Users/rkennedy/Dropbox/caol/me_as_professional/classes/2024_2025/geog4562/gis_data/2025"
+data_dir = "R:/2025/Spring/GEOG562/Students/cramerni/Lab5_nc/lab5_code"
 os.chdir(data_dir)
-print(os.getcwd())
+print(f"Working directory set to: {os.getcwd()}")
 
 
 ##################
@@ -26,14 +26,31 @@ print(os.getcwd())
 #   Set up a new smart raster class using rasterio  
 #    that will have a method called "calculate_ndvi"
 
+class SmartRaster:
+    def __init__(self, raster_path):
+        self.raster_path = raster_path
+        self.ds = rasterio.open(raster_path)
+        self.meta = self.ds.meta
+        self.bounds = self.ds.bounds
 
+    def calculate_ndvi(self, band4_index=4, band3_index=3, out_path="ndvi_output.tif"):
+        try:
+            nir = self.ds.read(band4_index).astype('float32')
+            red = self.ds.read(band3_index).astype('float32')
+            ndvi = (nir - red) / (nir + red + 1e-10)  # Prevent divide-by-zero
 
+            # Save NDVI as a GeoTIFF
+            meta = self.meta.copy()
+            meta.update(dtype='float32', count=1)
 
+            with rasterio.open(out_path, 'w', **meta) as dst:
+                dst.write(ndvi, 1)
 
-
-
-
-
+            print(f"NDVI written to {out_path}")
+            return True
+        except Exception as e:
+            print(f"NDVI calculation error: {e}")
+            return False
 
 
 ##################
@@ -43,9 +60,22 @@ print(os.getcwd())
 #    to calculate the zonal statistics for a raster
 #    and add them as a column to the attribute table of the vector
 
+class SmartVector:
+    def __init__(self, vector_path):
+        self.gdf = gpd.read_file(vector_path)
+        self.vector_path = vector_path
 
+    def zonal_stats_to_field(self, raster_path, stat="mean", field_name="NDVI_mean"):
+        try:
+            stats = zonal_stats(self.gdf, raster_path, stats=stat, geojson_out=True)
+            values = [f["properties"][stat] for f in stats]
+            self.gdf[field_name] = values
+            print(f"{field_name} added to GeoDataFrame")
+            return True
+        except Exception as e:
+            print(f"Zonal stats error: {e}")
+            return False
 
-
-
-
-
+    def save(self, out_path):
+        self.gdf.to_file(out_path)
+        print(f"GeoDataFrame saved to {out_path}")
--- nc_lab5_scripts.py
+++ nc_lab5_scripts.py
@@ -1,42 +1,32 @@
 # Lab 5 scripts
 
+import importlib
 import nc_lab5_functions as l5
 
-#  Part 1:
-
-#  Assign a variable to the Landsat file 
+importlib.reload(l5)
 
 
-# Pass this to your new smart raster class
+# PART 1: Smart Raster NDVI
+landsat_path = "Landsat_image_corv.tif"
+sr = l5.SmartRaster(landsat_path)
+
+# Calculate NDVI and save output
+sr.calculate_ndvi(out_path="ndvi_output.tif")
 
 
+# PART 2: Smart Vector Zonal Stats
+taxlots_path = "Benton_County_TaxLots.shp"
+sv = l5.SmartVector(taxlots_path)
 
-# Calculate NDVI and save to and output file
-
-
-
-
-
-
-# Part 2:
-# Assign a variable to the parcels data shapefile path
+# Calculate zonal stats and attach NDVI to parcels
+if sv.zonal_stats_to_field("ndvi_output.tif", field_name="NDVI_mean"):
+    sv.save("Benton_TaxLots_with_ndvi.shp")
 
 
-#  Pass this to your new smart vector class
+# PART 3: Optional Visualization
+import matplotlib.pyplot as plt
 
-
-#  Calculate zonal statistics and add to the attribute table of the parcels shapefile
-
-
-
-#  Part 3: Optional
-#  Use matplotlib to make a map of your census tracts with the average NDVI values
-
-
-
-
-
-
-
-
-
+sv.gdf.plot(column="NDVI_mean", cmap="YlGn", legend=True, edgecolor="k")
+plt.title("Average NDVI by Tax Lot")
+plt.axis("off")
+plt.show()
